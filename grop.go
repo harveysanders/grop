c2b33e402043ef8a226fc147b4e8f95eca628e15
Harvey Sanders <sanders.harvey@gmail.com>
2022-03-31 11:13:41 -0400

    run func to handle args

--- grop.go
+++ grop.go
@@ -1,8 +1,38 @@
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func Search(w io.Writer, r io.Reader, term string) error {
 
+	return nil
+}
+
+func run(args []string, w io.Writer, r io.Reader) error {
+	term := args[0]
+	fmt.Println(term)
+
+	if len(args) == 1 {
+		// Use stdin
+		err := Search(os.Stdout, os.Stdin, term)
+		if err != nil {
+			return err
+		}
+	}
+
+	// More than one arg, assume last arg is path to file
+	fp := args[len(args)-1]
+	file, err := os.Open(fp)
+	if err != nil {
+		return err
+	}
+
+	err = Search(os.Stdout, file, term)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -8,16 +8,8 @@ import (
 
 func main() {
 	var in io.Reader
-	term := os.Args[1]
 
-	// Use file as first input
-	fpath := os.Args[len(os.Args)-1]
-	if fpath == "" {
-		// Then fallback to stdin
-		in = os.Stdin
-	}
-
-	if err := Search(os.Stdout, in, term); err != nil {
+	if err := run(os.Args[1:], os.Stdout, in); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
