4ff83a99e92c721ac5625998d08ab18da700795c
Harvey Sanders <sanders.harvey@gmail.com>
2022-04-04 10:12:27 -0400

    Add --color flag

--- cmd/grop/main.go
+++ cmd/grop/main.go
@@ -9,9 +9,11 @@ import (
 	"github.com/harveysanders/grop"
 )
 
+var color string
 var ignoreCase bool
 
 func init() {
+	flag.StringVar(&color, "color", "never", "when to highlight matching patterns ( always | auto | never )")
 	flag.BoolVar(&ignoreCase, "i", false, "case insensitive match")
 
 	flag.Usage = usage
@@ -27,12 +29,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := grop.Run(args, os.Stdout, in, grop.Options{IgnoreCase: ignoreCase}); err != nil {
+	opts := grop.Options{IgnoreCase: ignoreCase, WhenHighlight: color}
+
+	if err := grop.Run(args, os.Stdout, in, opts); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
 }
 
 func usage() {
-	fmt.Fprintln(os.Stderr, "usage: grop [-i] [pattern] [file]")
+	fmt.Fprintln(os.Stderr, "usage: grop [-i] [--color=when] [pattern] [file]")
 }
--- grop.go
+++ grop.go
@@ -8,7 +8,8 @@ import (
 )
 
 type Options struct {
-	IgnoreCase bool
+	IgnoreCase    bool
+	WhenHighlight string // TODO: Use enum
 }
 
 func Search(w io.Writer, r io.Reader, term string, o Options) error {
@@ -38,8 +39,8 @@ func Search(w io.Writer, r io.Reader, term string, o Options) error {
 
 		if loc != nil && loc[0] > -1 {
 			// Write highlighted match back to Writer
-			match := colorizeMatch(l, loc[0], loc[1], Red)
-			_, err := io.WriteString(w, match)
+			match := highlightMatch(l, loc[0], loc[1], Red, o.WhenHighlight)
+			_, err := io.WriteString(w, match+"\n")
 			if err != nil {
 				return err
 			}
@@ -75,6 +76,15 @@ func Run(args []string, w io.Writer, r io.Reader, opts Options) error {
 	return nil
 }
 
-func colorizeMatch(s string, start, end int, color Color) string {
-	return s[:start] + color.String() + s[start:end] + Reset.String() + s[end:]
+func highlightMatch(s string, start, end int, color Color, when string) string {
+	switch when {
+	case "always":
+		return s[:start] + color.String() + s[start:end] + Reset.String() + s[end:]
+	case "auto":
+		// TODO: Probably need to do something different here
+		return s[:start] + color.String() + s[start:end] + Reset.String() + s[end:]
+	case "never":
+		return s
+	}
+	return s
 }
